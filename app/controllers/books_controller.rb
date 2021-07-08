312e0b0c369dc2c535e37c777dbe1e32fe11ccea
AyaOb <Skilpro2162@gmail.com>
2021-07-08 11:37:58 +0000

    add controller-index,create,book_params & add View-index,form_with

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
@@ -7,4 +15,10 @@ class BooksController < ApplicationController
 
   def show
   end
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+end
+
 end
