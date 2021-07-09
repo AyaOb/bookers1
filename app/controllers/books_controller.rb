356cf64338cfb802e6f3065e9ca53c88d1b868bb
AyaOb <Skilpro2162@gmail.com>
2021-07-09 06:42:36 +0000

    [Add] editView and controller-edit,update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,18 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
 
-  def edit
-  end
-
   def show
     @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
 private
